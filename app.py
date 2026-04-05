2abe1cf6db2b9fd50cf4cfaced98cecb24fde3e6
ajnaajna <ajna.cuhi@gmail.com>
2026-04-05 16:21:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -107,4 +107,5 @@ if page == "Main":
     st.subheader("ℹ️ Viri podatkov")
     st.write("Aplikacija združuje GURS (Prostorski portal), ARSO (Vremenske storitve) in OPSI/Promet.si (Stanje na cestah).")
 elif page == "Floods":
-    import pages.floods_part_ajna
+    from pages import floods_part_ajna
+    floods_part_ajna.render()
